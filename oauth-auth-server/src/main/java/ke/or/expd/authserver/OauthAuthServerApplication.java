263a4d0820838fe1fec32ea9a411475cf130e98e
otienochris <otienochris98@gmail.com>
2022-12-12 10:23:14 +0300

    adding auth server configs

--- oauth-auth-server/src/main/java/ke/or/expd/authserver/OauthAuthServerApplication.java
+++ oauth-auth-server/src/main/java/ke/or/expd/authserver/OauthAuthServerApplication.java
@@ -2,12 +2,20 @@ package ke.or.expd.authserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class OauthAuthServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OauthAuthServerApplication.class, args);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(11);
     }
 
 }
