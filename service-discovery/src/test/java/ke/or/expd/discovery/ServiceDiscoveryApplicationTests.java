1f3b2c0f8cef844875a53dbce4fe47c31ac55702
otienochris <otienochris98@gmail.com>
2023-04-09 07:17:56 +0300

    feature: adding discovery client

---
+++ service-discovery/src/test/java/ke/or/expd/discovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package ke.or.expd.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
